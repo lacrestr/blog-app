d758e542baee3b3aa3f99e9ab9c55a74e4c22b1b
LaCrest Reed <lacrestreed@gmail.com>
2018-03-26 20:47:30 +0000

    added CRUD and categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,41 +1,11 @@
 class PostsController < ApplicationController
    before action :find_post, only:[:show, :edit, :update, :destroy]
-    
-    def index 
-        @posts = Post.all
-end
-    def show
-end
-    def new
-        @posts = Post.new
-    end
-    def create
-        @post = Post.new(posts_params)
-        if @post.save
-            redirect_to post, notice: "The post was created"
-        else
-            render 'new'
-        end
-    end
-    def edit
-    end
-    def update 
-         @post = Post.update(posts_params)
-        redirect_to post, notice: "The update was sucessful"
-    else
-        render 'edit'
-    end
-    def destroy 
-        @post.destroy
-        redirect_to root_path, notice: "Post destroyed"
-    end
-private
-    def posts_params
-        params.require( :post).permit(:title, :content, :category_id)
-    end
-    def find_posts
-        @post = Post.find(params[:id])
-    end
-end
-        
-       
+          
+      def index
+if params[:category].blank?
+  @posts = Post.all.order("created_at DESC")
+  else
+  @category_id = Category.find_by(name: params[:category]).id
+  @posts = Post.where(category_id: @category_id).order("created_at DESC")
+  end
+end 
